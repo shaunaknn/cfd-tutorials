98d1d401c2e0bb7d5ba961da63cef245af335c74
shaunaknn <shaunaknn@gmail.com>
2025-08-13 19:20:56 +0530

    separated linear and nonlinear convection

--- linConvection1D.py
+++ linConvection1D.py
@@ -1,11 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def nonlinearconvection(nx,nt):
+def linearconvection(nx,nt):
     # nx = no of grid points, nt = no of time steps
     l = 2 # domain length
+    cfl = 0.5 # CFL number
+    c = 1 # wave speed
     dx = l/(nx-1) # delta x
-    dt = nt/1000 # delta t
+    dt = cfl*dx/c # delta t
 
     u = np.ones(nx) # solution array
     u[int(.5/dx):int(1/dx+1)] = 2 # initial condition
@@ -14,9 +16,9 @@ def nonlinearconvection(nx,nt):
 
     for n in range(nt): # solving in time
         un = u.copy()
-        u[1:] = un[1:]-un[1:]*dt/dx*(un[1:]-un[:-1])
+        u[1:] = un[1:]-c*dt/dx*(un[1:]-un[:-1])
 
     plt.plot(np.linspace(0,l,nx),u) # plot after nt
     plt.show()
 
-nonlinearconvection(41,20)
+linearconvection(41,20)

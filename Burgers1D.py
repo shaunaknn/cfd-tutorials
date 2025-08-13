85a9089ca3554253dfef3c75cb69847345bab508
shaunaknn <shaunaknn@gmail.com>
2025-08-13 11:48:34 +0530

    vectorized for loop

--- Burgers1D.py
+++ Burgers1D.py
@@ -27,8 +27,9 @@ def burgers(nx,nt):
 
     for n in range(nt):
         un = u.copy()
-        for i in range(1,nx-1):
-            u[i] = un[i] - un[i]*dt/dx*(un[i]-un[i-1]) + nu*(dt/dx**2)*(un[i+1]-2*un[i]+un[i-1])
+        u[1:-1] = un[1:-1] - un[1:-1]*dt/dx*(un[1:-1]-un[:-2]) + nu*(dt/dx**2)*(un[2:]-2*un[1:-1]+un[:-2])
+        #for i in range(1,nx-1):
+        #    u[i] = un[i] - un[i]*dt/dx*(un[i]-un[i-1]) + nu*(dt/dx**2)*(un[i+1]-2*un[i]+un[i-1])
         u[0] = un[0] - un[0]*dt/dx*(un[0]-un[-2]) + nu*(dt/dx**2)*(un[1]-2*un[0]+un[-2])
         u[-1] = u[0]
 

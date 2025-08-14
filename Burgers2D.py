2733a9920f1aa02615fad14e03b5f3cde0680955
shaunaknn <shaunaknn@gmail.com>
2025-08-14 08:46:55 +0530

    Burgers equation in 2D

---
+++ Burgers2D.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+def plot_2dfield(X,Y,field,field2,title):
+    fig = plt.figure(figsize=(11,7),dpi=100)
+    ax = fig.add_subplot(111,projection='3d')
+    ax.plot_surface(X,Y,field,cmap=cm.viridis)
+    ax.plot_surface(X,Y,field2,cmap=cm.viridis)
+
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$y$')
+    ax.set_title(title)
+    plt.show()
+
+def burgers2d(nt):
+    for n in range(nt+1):
+        un = u.copy()
+        vn = v.copy()
+
+        u[1:-1,1:-1] = un[1:-1,1:-1] - un[1:-1,1:-1]*dt/dx*(un[1:-1,1:-1]-un[1:-1,:-2])\
+              - vn[1:-1,1:-1]*dt/dy*(un[1:-1,1:-1]-un[:-2,1:-1])\
+                + nu*dt/dx**2*(un[1:-1,2:]-2*un[1:-1,1:-1]+un[1:-1,:-2])\
+                    + nu*dt/dy**2*(un[2:,1:-1]-2*un[1:-1,1:-1]+un[:-2,1:-1])
+        
+        v[1:-1,1:-1] = vn[1:-1,1:-1] - un[1:-1,1:-1]*dt/dx*(vn[1:-1,1:-1]-vn[1:-1,:-2])\
+            - vn[1:-1,1:-1]*dt/dy*(vn[1:-1,1:-1]-vn[:-2,1:-1])\
+                + nu*dt/dx**2*(vn[1:-1,2:]-2*vn[1:-1,1:-1]+vn[1:-1,:-2])\
+                    + nu*dt/dy**2*(vn[2:,1:-1]-2*vn[1:-1,1:-1]+vn[:-2,1:-1])
+
+        u[0,:], u[-1,:], u[:,0], u[:,-1] = 1, 1, 1, 1
+        v[0,:], v[-1,:], v[:,0], v[:,-1] = 1, 1, 1, 1
+
+    plot_2dfield(X,Y,u,v,'Solution at final time step')
+
+#nx, ny = no of grid points, nt = no of time steps
+lx, ly = 2, 2 #domain dimensions
+nx, ny = 41, 41
+nt = 120 #number of timesteps
+dx = lx/(nx-1) #delta x
+dy = ly/(ny-1) #delta y
+
+nu = 0.01 #viscosity
+sigma = 0.0009 #stability parameter
+dt = sigma*dx*dy/nu #delta t
+
+x = np.linspace(0,lx,nx)
+y = np.linspace(0,ly,ny)
+X,Y = np.meshgrid(x,y)
+
+u = np.ones((ny,nx)) #solution arrays
+v = np.ones((ny,nx))
+un = np.ones_like(u) #temp arrays
+vn = np.ones_like(u)
+comb = np.ones((ny,nx))
+
+u[int(.5/dy):int(1/dy+1),int(.5/dx):int(1/dx+1)] = 2 #initial condition
+v[int(.5/dy):int(1/dy+1),int(.5/dx):int(1/dx+1)] = 2 #initial condition
+
+burgers2d(nt)

a92660bd75e381e2a87e7c2891dceb88b2fbd776
shaunaknn <shaunaknn@gmail.com>
2025-08-20 18:59:12 +0530

    more readable code

--- 12Steps_NS/linConvection1D.py
+++ 12Steps_NS/linConvection1D.py
@@ -1,24 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def linearconvection(nx,nt):
-    # nx = no of grid points, nt = no of time steps
-    l = 2 # domain length
-    cfl = 0.5 # CFL number
-    c = 1 # wave speed
-    dx = l/(nx-1) # delta x
-    dt = cfl*dx/c # delta t
+l = 2 # domain length
+nx = 41 # no of grid points
+nt = 20 # no of timesteps
+cfl = 0.5 # CFL number
+c = 1 # wave speed
+dx = l/(nx-1) # delta x
+dt = cfl*dx/(abs(c)+1e-6) # delta t
 
-    u = np.ones(nx) # solution array
-    u[int(.5/dx):int(1/dx+1)] = 2 # initial condition
+u = -1*np.ones(nx) # solution array
+u[int(.5/dx):int(1/dx+1)] = 2 # initial condition
 
-    un = np.ones(nx) # temp array
-
+def linearconvection(u,nt,dx,dt):
+    un = np.ones_like(u)
     for n in range(nt): # solving in time
         un = u.copy()
         u[1:] = un[1:]-c*dt/dx*(un[1:]-un[:-1])
 
-    plt.plot(np.linspace(0,l,nx),u) # plot after nt
-    plt.show()
+    return u
 
-linearconvection(41,20)
+plt.plot(np.linspace(0,l,nx),u,label='initial solution') # initial solution
+plt.legend()
+plt.show()
+
+linearconvection(u,nt,dx,dt)
+plt.plot(np.linspace(0,l,nx),u,label='standard upwind') # plot after nt
+plt.legend()
+plt.show()

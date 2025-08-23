69b5e287559a3ac389b90fe13227d8ec623a7151
shaunaknn <shaunaknn@gmail.com>
2025-08-23 18:01:47 +0530

    modular code

--- 12Steps_NS/CavityFlow.py
+++ 12Steps_NS/CavityFlow.py
@@ -10,34 +10,28 @@ dx = lx/(nx-1) #deltax
 dy = ly/(ny-1) #deltay
 x = np.linspace(0,lx,nx) #x-domain array
 y = np.linspace(0,ly,ny) #y-domain array
+c = 1 #lid velocity
 
 rho = 1 #density
 nu = 0.1 #kinematic viscosity
 dt = 0.001 #timestep size
+Re = c*max(lx,ly)/nu #Reynolds number
+l1norm_target = 1e-4
 
 u = np.zeros((ny,nx))
 v = np.zeros((ny,nx))
 p = np.zeros((ny,nx))
 
-def plotcontour(x,y,field,title): #plots velocity vectors
+def plotfield(x,y,field,u,v,title,mode='quiver'): #plots velocity vectors
     plt.figure(figsize=(11,7),dpi=100)
     X,Y = np.meshgrid(x,y)
     plt.contourf(X,Y,field,alpha=0.5,cmap=cm.viridis)
     plt.colorbar()
     plt.contour(X,Y,field,cmap=cm.viridis)
-    plt.quiver(X[::2,::2],Y[::2,::2],u[::2,::2],v[::2,::2])
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title(title)
-    plt.show()
-
-def streamlines(x,y,field,title): #plots velocity streamlines
-    plt.figure(figsize=(11,7),dpi=100)
-    X,Y = np.meshgrid(x,y)
-    plt.contourf(X,Y,field,alpha=0.5,cmap=cm.viridis)
-    plt.colorbar()
-    plt.contour(X,Y,field,cmap=cm.viridis)
-    plt.streamplot(X,Y,u,v)
+    if mode == 'quiver':
+        plt.quiver(X[::2,::2],Y[::2,::2],u[::2,::2],v[::2,::2])
+    elif mode == 'stream':
+        plt.streamplot(X,Y,u,v)
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.xlim(x.min(),x.max())
@@ -53,7 +47,7 @@ def buildb(b,u,v,dx,dy,dt,rho): #separate source term of p poisson equation
     
     return b
 
-def ppoison(p,dx,dy,b):
+def ppoisson(p,dx,dy,b): # no. of iterations specified version
     pn = np.empty_like(p)
 
     for q in range(nit):
@@ -68,7 +62,55 @@ def ppoison(p,dx,dy,b):
 
     return p
 
-def cavity(u,v,p,nt,dx,dy,dt,rho,nu):
+def ppoissonl1(p,dx,dy,b,l1norm_target): #l1 norm target version
+    l1norm = 1
+    itercount = 0
+    eta = 1e-8
+    pn = np.empty_like(p)
+
+    while l1norm>l1norm_target:
+        pn = p.copy()
+        p[1:-1,1:-1] = ((pn[1:-1,2:]+pn[1:-1,:-2])*dy**2 + (pn[2:,1:-1]+pn[:-2,1:-1])*dx**2)/(2*(dx**2 + dy**2))\
+                        -dx**2 * dy**2/(2*(dx**2 + dy**2)) * b[1:-1,1:-1]
+        
+        p[-1,:] = 0 #p=0 at y=2
+        p[0,:] = p[1,:] #dp/dy=0 at y=0
+        p[:,-1] = p[:,-2] #dp/dx=0 at x=2
+        p[:,0] = p[:,1] #dp/dx=0 at x=0
+
+        l1norm = (np.sum(np.abs(p - pn)))/np.sum(np.abs(pn)+eta)
+        itercount += 1
+
+    return p, itercount
+
+def update_u(u,un,vn,p,dx,dy,dt,rho,nu): #update u velocity
+    u[1:-1,1:-1] = un[1:-1,1:-1] - un[1:-1,1:-1]* dt/dx *(un[1:-1,1:-1]-un[1:-1,:-2])\
+                        -vn[1:-1,1:-1]* dt/dy *(un[1:-1,1:-1]-un[:-2,1:-1])\
+                        -dt/(2*rho*dx) * (p[1:-1,2:]-p[1:-1,:-2])\
+                        + nu*(dt/dx**2 * (un[1:-1,2:] - 2*un[1:-1,1:-1] + un[1:-1,:-2])\
+                              + dt/dy**2 * (un[2:,1:-1] - 2*un[1:-1,1:-1] + un[:-2,1:-1]))
+    return u
+
+def update_v(v,un,vn,p,dx,dy,dt,rho,nu): #update v velocity
+    v[1:-1,1:-1] = vn[1:-1,1:-1] - un[1:-1,1:-1]* dt/dx *(vn[1:-1,1:-1]-vn[1:-1,:-2])\
+                        -vn[1:-1,1:-1]* dt/dy *(vn[1:-1,1:-1]-vn[:-2,1:-1])\
+                        -dt/(2*rho*dy) * (p[2:,1:-1]-p[:-2,1:-1])\
+                        + nu*(dt/dx**2 * (vn[1:-1,2:] - 2*vn[1:-1,1:-1] + vn[1:-1,:-2])\
+                              + dt/dy**2 * (vn[2:,1:-1] - 2*vn[1:-1,1:-1] + vn[:-2,1:-1]))
+    return v
+
+def applyBC(u,v,c): #apply boundary conditions
+    u[0,:] = 0 #bottom wall
+    u[:,0] = 0 #left wall
+    u[:,-1] = 0 #right wall
+    u[-1,:] = c #lid
+    v[0,:] = 0 #bottom wall
+    v[-1,:] = 0 #lid
+    v[:,0] = 0 #left wall
+    v[:,-1] = 0 #right wall
+    return u,v
+
+def cavity(u,v,p,nt,dx,dy,dt,rho,nu): #solve cavity flow
     un = np.empty_like(u)
     vn = np.empty_like(v)
     b = np.zeros((ny,nx))
@@ -77,32 +119,17 @@ def cavity(u,v,p,nt,dx,dy,dt,rho,nu):
         un[:] = u #similar to un = u.copy()
         vn[:] = v
         
-        b = buildb(b,u,v,dx,dy,dt,rho)
-        p = ppoison(p,dx,dy,b)
+        b = buildb(b,u,v,dx,dy,dt,rho) #build RHS of pressure-poisson eqn
+        #p = ppoisson(p,dx,dy,b)
+        p,itercount = ppoissonl1(p,dx,dy,b,l1norm_target)
 
-        u[1:-1,1:-1] = un[1:-1,1:-1] - un[1:-1,1:-1]* dt/dx *(un[1:-1,1:-1]-un[1:-1,:-2])\
-                        -vn[1:-1,1:-1]* dt/dy *(un[1:-1,1:-1]-un[:-2,1:-1])\
-                        -dt/(2*rho*dx) * (p[1:-1,2:]-p[1:-1,:-2])\
-                        + nu*(dt/dx**2 * (un[1:-1,2:] - 2*un[1:-1,1:-1] + un[1:-1,:-2])\
-                              + dt/dy**2 * (un[2:,1:-1] - 2*un[1:-1,1:-1] + un[:-2,1:-1]))
-        
-        v[1:-1,1:-1] = vn[1:-1,1:-1] - un[1:-1,1:-1]* dt/dx *(vn[1:-1,1:-1]-vn[1:-1,:-2])\
-                        -vn[1:-1,1:-1]* dt/dy *(vn[1:-1,1:-1]-vn[:-2,1:-1])\
-                        -dt/(2*rho*dy) * (p[2:,1:-1]-p[:-2,1:-1])\
-                        + nu*(dt/dx**2 * (vn[1:-1,2:] - 2*vn[1:-1,1:-1] + vn[1:-1,:-2])\
-                              + dt/dy**2 * (vn[2:,1:-1] - 2*vn[1:-1,1:-1] + vn[:-2,1:-1]))
+        u = update_u(u,un,vn,p,dx,dy,dt,rho,nu)
+        v = update_v(v,un,vn,p,dx,dy,dt,rho,nu)
 
-        u[0,:] = 0 #bottom
-        u[:,0] = 0 #left wall
-        u[:,-1] = 0 #right wall
-        u[-1,:] = 1 #lid
-        v[0,:] = 0 #bottom
-        v[-1,:] = 0 #lid
-        v[:,0] = 0 #left wall
-        v[:,-1] = 0 #right wall
+        u,v = applyBC(u,v,c)
 
     return u, v, p
 
 u,v,p = cavity(u,v,p,nt,dx,dy,dt,rho,nu)
-plotcontour(x,y,p,'Final time step velocity vectors')
-streamlines(x,y,p,'Streamlines at final time step')
+plotfield(x,y,p,u,v,'Final time step velocity vectors','quiver')
+plotfield(x,y,p,u,v,'Streamlines at final time step','stream')
